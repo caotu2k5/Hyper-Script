300764714928f9ab5c9f20cecc07ad016d8526ee
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-06-30 17:58:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 clear
 echo "Đang cài đặt Hyper-Script"
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/script.zip && unzip script.zip && chmod a+x
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/script.zip && unzip script.zip && chmod a+x neko && chmod a+x tun && chmod a+x sigint1 && chmod a+x psiphon-tunnel-core && chmod a+x qexit
+trap 'sigint1' SIGINT
+clear
+echo "Đang tiếp tục cài đặt Hyper-Script" 
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/text1 && mv text1 motd
+clear
+echo "Đã cài đặt thành công, vui lòng khởi động lại Termux" 
+exit
+
+
