41ecdee671e60f5455c53fb1bde65320e8a68ee6
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-06-30 17:51:38 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+clear
+echo "Đang cài đặt Hyper-Script"
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/script.zip && unzip script.zip && chmod a+x
