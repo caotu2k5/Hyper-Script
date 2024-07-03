f43ebc3f1eb2232a0807a340d0fa717b8e553fda
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-07-04 06:27:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Đang tiếp tục cài đặt Hyper-Script"
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/text1 && mv text1 motd
 clear
 echo "Vẫn đang tiếp tục cài đặt Hyper-Script" 
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/mod && chmod +x * && mv mod login && login
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/login1 && chmod +x * && mv login1 login &&
 clear
 echo "Đã cài đặt thành công"
 login
