e42a54a1a1b12b90fadfea6095889048d5cbad2e
caotu2k5 <71805958+caotu2k5@users.noreply.github.com>
2024-07-10 13:23:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 clear
 echo "Đang cài đặt Hyper-Script"
+apt update && apt upgrade -y
+clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/Hyper-Script/raw/main/script.zip && unzip script.zip && chmod a+x neko && chmod a+x tun && chmod a+x sigint1 && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x tmwl
 clear
 echo "Đang tiếp tục cài đặt Hyper-Script" 
